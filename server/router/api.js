1b0c521a34c92c3f6dee3c3eda7b09a10e94db1f
mikatpt <mikatpt@gmail.com>
2020-11-28 14:14:25 -0500

    Refactored repetitive router code to use loops instead

--- server/router/api.js
+++ server/router/api.js
@@ -1,15 +1,14 @@
 const { Router } = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware: CPM } = require('http-proxy-middleware');
 const {
   service1, service2, service3, service4,
 } = require('../config/services.js');
 
 const router = Router();
 
-router.use(service1.api[0], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
-router.use(service1.api[1], createProxyMiddleware({ target: service1.url, changeOrigin: true }));
-router.use(service2.api, createProxyMiddleware({ target: service2.url, changeOrigin: true }));
-router.use(service3.api, createProxyMiddleware({ target: service3.url, changeOrigin: true }));
-router.use(service4.api, createProxyMiddleware({ target: service4.url, changeOrigin: true }));
+service1.api.forEach((api) => router.use(api, CPM({ target: service1.url, changeOrigin: true })));
+service2.api.forEach((api) => router.use(api, CPM({ target: service2.url, changeOrigin: true })));
+router.use(service3.api, CPM({ target: service3.url, changeOrigin: true }));
+router.use(service4.api, CPM({ target: service4.url, changeOrigin: true }));
 
 module.exports = router;
